9a17ba5b712e9463a2d588e49cad3e9ecee07236
Edmond Jajaga <edmond.jajaga@gmail.com>
2024-07-18 19:24:22 +0200

    fixed-gitignore3

--- obj/Debug/net8.0/EdumindAkademia.AssemblyInfo.cs
+++ obj/Debug/net8.0/EdumindAkademia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EdumindAkademia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df304ed24ca166a98839c71c5e8379485e6768b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f8ff9d238fe3838f04e0c5a18b37af7530e6382")]
 [assembly: System.Reflection.AssemblyProductAttribute("EdumindAkademia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EdumindAkademia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
