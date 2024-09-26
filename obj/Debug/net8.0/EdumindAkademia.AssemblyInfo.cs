663fa2a1515027e3ad2346e20a29b122b3ec9a83
Edmond Jajaga <edmond.jajaga@gmail.com>
2024-09-26 21:02:39 +0200

    cm

--- obj/Debug/net8.0/EdumindAkademia.AssemblyInfo.cs
+++ obj/Debug/net8.0/EdumindAkademia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EdumindAkademia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300842e5eb2dc879765a3f5631064a1e5d7a78f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81531ce36f08cba70e6c8686e8d85fe7e9428abb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EdumindAkademia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EdumindAkademia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
